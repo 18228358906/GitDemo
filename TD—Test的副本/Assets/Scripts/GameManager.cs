ffbed89255e77f3e88f36c70b7d46abdc5a784d5
18228358906 <1055295121@qq.com>
2020-05-01 15:47:04 +0800

    wzh

--- TD—Test的副本/Assets/Scripts/GameManager.cs
+++ TD—Test的副本/Assets/Scripts/GameManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject GameOverUI;
+    public GameObject GameWinUI;
+    public GameObject MusicUI;
     public static GameManager Instance;
     public Text endMassage;
+    public Text winMassage;
     private EnemySpawner enemySpawner;
     // Start is called before the first frame update
     void Awake()
@@ -20,12 +24,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     public void Win()
     {
-        GameOverUI.SetActive(true);
-        endMassage.text = "胜 利";
+        GameWinUI.SetActive(true);
+        winMassage.text = "胜 利";
     }
     public void Faild()
     {
@@ -40,5 +44,21 @@ public class GameManager : MonoBehaviour
     public void OnButtonMenu()
     {
         SceneManager.LoadScene(0);
+    }
+    public void OnButtonNext2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void OnButtonNext3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void OnButtonNext4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void OnMusicUI()
+    {
+        MusicUI.SetActive(true);
     }
 }
