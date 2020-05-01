11933724931aae46671eb5e54c1b4acb7fcb6bf5
Snow1357375155 <63908637+Snow1357375155@users.noreply.github.com>
2020-05-01 16:42:16 +0800

    修改了开始主菜单界面

---
+++ TD—Test的副本/Assets/Scripts/MainMenuScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScene : MonoBehaviour
+{
+    public GameObject CourseUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GameStartButton()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    public void GameSettingButton()
+    {
+
+    }
+    public void OnCourseButton()
+    {
+        CourseUI.SetActive(true);
+    }
+    public void OnCouseRuturnButton()
+    {
+        CourseUI.SetActive(false);
+    }
+
+    public void GameExitButton()
+    {
+        Application.Quit();
+    }
+}
